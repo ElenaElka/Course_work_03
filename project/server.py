7bbbd06d611102247cf041ed8945b6586d9a107a
peshkovaelena <peshkovaes@bk.ru>
2022-04-30 21:53:10 +0700

    Commit_02. Correct mistakes_2

--- project/server.py
+++ project/server.py
@@ -4,6 +4,9 @@ from flask_restx import Api
 
 from project.setup_db import db
 from project.views import genres_ns
+from project.views.users import users_ns
+from project.views.directors import directors_ns
+from project.views.movies import movies_ns
 
 api = Api(
     authorizations={
@@ -26,6 +29,9 @@ def create_app(config_obj):
     api.init_app(app)
 
     # Регистрация эндпоинтов
+    api.add_namespace(directors_ns)
     api.add_namespace(genres_ns)
+    api.add_namespace(movies_ns)
+    api.add_namespace(users_ns)
 
     return app
--- project/views/__init__.py
+++ project/views/__init__.py
@@ -1,5 +1,11 @@
 from .genres import genres_ns
+from .directors import directors_ns
+from .movies import movies_ns
+from .users import users_ns
 
 __all__ = [
+    "directors_ns",
     "genres_ns",
+    "movies_ns",
+    "users_ns",
 ]
